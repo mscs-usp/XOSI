65e0a4103e4697d7a8ff47d834dd85ca389cf197
Samuel Kortas <skortas@paramount.rennes.grid5000.fr>
2009-11-28 14:18:36 +0100

    .

--- install_g5k.py
+++ install_g5k.py
@@ -1,10 +1,22 @@
 
 import os
+from socket import *
 
 nodes_file = os.getenv("OAR_FILE_NODES")
 
 print nodes_file
 
-f = open(nodes_files,f)
-nodes = f.readlines()
+f = open(nodes_file,"r")
+nodes_all = f.readlines()
 f.close()
+
+nodes = {}
+
+for n in nodes_all:
+  if not n in nodes.keys():
+     print n	
+     res  = os.system(" gethostip "+n)
+     print str(res).split(' ')
+     nodes[n] = addr+" "+name
+
+print nodes
