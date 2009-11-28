6fe4796aae2e8e83b52fb33e9666f10beedfce00
Samuel Kortas <skortas@paramount.rennes.grid5000.fr>
2009-11-28 22:29:43 +0100

    .

--- install_g5k.py
+++ install_g5k.py
@@ -137,7 +137,7 @@ for n in alias:
      ssh root@__HOST__ 'urpmi emacs'
      ssh root@__HOST__ 'git clone ssh://skortas@paramount/home/orsay/skortas/XOSI'
      ssh root@__HOST__ 'git clone ssh://skortas@paramount/home/orsay/skortas/XOST'
-     ssh root@__HOST__ 'ln -s XOSI/CONF/emacs.g5j .emacs'
+     ssh root@__HOST__ 'ln -s XOSI/CONF/emacs.g5k .emacs'
      ssh root@__HOST__ 'ln -s XOSI/CONF/bashrc-xos .bashrc-xos'
      ssh root@__HOST__ 'echo "export XOSHOST=__ALIAS__" >> .bashrc'
      ssh root@__HOST__ 'echo ". .bashrc-xos" >> .bashrc'
