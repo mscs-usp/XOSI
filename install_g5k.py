b8e1a16b865c28d8ceaa6240b9768b4625d2d6d8
Samuel Kortas <skortas@paramount.rennes.grid5000.fr>
2009-11-28 18:08:24 +0100

    .

--- install_g5k.py
+++ install_g5k.py
@@ -11,11 +11,27 @@ nodes_all = f.readlines()
 f.close()
 
 nodes = {}
+alias = {}
+xos0 = False
+
+inode=0
 
 for n in nodes_all:
   if not n in nodes.keys():
      n=n[:-1]
      res  = socket.gethostbyname(n)
      nodes[n] = res
+     if not xos0:
+	xos0=n
+     alias[n]="xos%d"%inode
+     inode=inode+1
+     	
+
+# construction de hosts
+
+hosts = "127.0.0.1 localhost"
+
+for n in nodes:
+   
 
 print nodes
